55b7e5211278ec8bfe59013f1c4e90fddd0b3b19
Etienne Samson <samson.etienne@gmail.com>
2018-07-28 23:30:39 +0000

    Initial Commit

---
+++ ZFS/Test.m
@@ -0,0 +1,19 @@
+//
+//  Test.m
+//  ZFS Utility
+//
+//  Created by Etienne on 28/07/2018.
+//  Copyright © 2018 Etienne Samson. All rights reserved.
+//
+
+//#import <Foundation/Foundation.h>
+
+//#include "External/spl/spl_config.h"
+//#include "External/zfs/zfs_config.h"
+
+//#include <stdarg.h>
+//#include <sys/types.h>
+
+#include <libzfs.h>
+
+///System/Library/Frameworks/Kernel.framework/Versions/A/Headers External/spl/include External/zfs/include
---
+++ ZFS/ZFS.h
@@ -0,0 +1,17 @@
+//
+//  ZFS.h
+//  ZFS
+//
+//  Created by Etienne on 28/07/2018.
+//  Copyright © 2018 Etienne Samson. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ZFS.
+FOUNDATION_EXPORT double ZFSVersionNumber;
+
+//! Project version string for ZFS.
+FOUNDATION_EXPORT const unsigned char ZFSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZFS/PublicHeader.h>
